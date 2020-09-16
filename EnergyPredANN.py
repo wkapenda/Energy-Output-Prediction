304e90804e5e9974579b218d39eb99e33ab93531
Willem <38408005+wkapenda@users.noreply.github.com>
2020-09-16 13:40:00 +0200

    Update EnergyPredANN.py
    Added comments

--- EnergyPredANN.py
+++ EnergyPredANN.py
@@ -4,29 +4,37 @@ import pandas as pd
 # Read data from the excel file and store it into a dataset
 dataset = pd.read_csv('Folds5x2_pp.csv')
 
+# Create the features (x) and target (y) data sets
 x = dataset.iloc[:, : -1].values
 y = dataset.iloc[:, -1].values
 
+# import sklearn machine learning library
 from sklearn.model_selection import train_test_split
 
+# Split the features and target datasets into training and test datasets
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
 
+# Import the tensorflow machine learning library to create the ANN model
 import tensorflow as tf
 
+# Initialize the ANN model
 ann = tf.keras.models.Sequential()
 
+# Add dense layers to create an ANN model with an architecture of 4-6-6-1
 ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
 ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
 ann.add(tf.keras.layers.Dense(units=1, activation= None))
 
+# Compile the ANN model using the adam optimizer and the mean squared error loss function
 ann.compile(optimizer='adam', loss='mean_squared_error')
 
+# Fit the training data into the ANN model, using epochs = 100
 ann.fit(X_train, y_train, batch_size=32, epochs=100)
 
+# Use the test data to make the prediction
 y_pred = ann.predict(X_test)
 
+# Set the precision to two decimal places
 np.set_printoptions(precision=2)
 
-print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
-
-print(y)
+ann.summary()
